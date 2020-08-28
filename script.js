c79eb9a1adb02437cb1a6ba8b604a5a42f88b700
Sirbelog <sirbelog@mail.ru>
2020-08-28 13:36:39 +0900

    слайдер и доп

--- script.js
+++ script.js
@@ -8,8 +8,29 @@ let prevButton = document.querySelector('#prev__button')
 let images = document.querySelectorAll(".gallery__list img")
 
 let i = 0;
+let timer
+timer = setInterval(nextSlide,4000)
 
+
+
+nextButton.addEventListener("click", nextSlide)
 nextButton.addEventListener("click", function(){
+	clearInterval(timer)
+	timer = setInterval(nextSlide,4000)
+})
+
+prevButton.addEventListener("click", function(){
+	images[i].className = "";
+	i--;
+	if(i<0){
+		i=images.length-1;
+	};
+	images[i].className = "gallery__main"
+})		
+
+
+function nextSlide(){
+
 	images[i].className = "";
 	i++;
 
@@ -18,17 +39,9 @@ nextButton.addEventListener("click", function(){
 	};
 
 	images[i].className = "gallery__main"
-	
-})
 
-prevButton.addEventListener("click", function(){
-	images[i].className = "";
-	i--;
+}
 
-	if(i<0){
-		i=images.length-1;
-	};
 
-	images[i].className = "gallery__main"
-	
-})
+
+
